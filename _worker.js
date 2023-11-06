91d822f56123f193d37dd6f9a75fa3ec2a6c8cb7
frgmx <137780672+frgmx@users.noreply.github.com>
2023-11-06 18:59:23 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '8fb25760-1075-4742-8e44-766617baeac5';
 
-let proxyIP = '';
+let proxyIP = '216.238.108.127';
 
 // The user name and password do not contain special characters
 // Setting the address will ignore proxyIP
